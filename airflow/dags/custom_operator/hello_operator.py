833ac3fe38536180d0d5e0cd1904fdd6189faaf5
Prashant15887 <SINGHPRASHANT87@GMAIL.COM>
2025-03-06 11:54:25 +0530

    Added UI customisation for custom operator.

--- airflow/dags/custom_operator/hello_operator.py
+++ airflow/dags/custom_operator/hello_operator.py
@@ -2,6 +2,10 @@ from airflow.models.baseoperator import BaseOperator
 
 
 class HelloOperator(BaseOperator):
+    ui_color = "#ff0000"
+    ui_fgcolor = "#000000"
+    custom_operator_name = "Howdy"
+
     def __init__(self, name: str, **kwargs) -> None:
         super().__init__(**kwargs)
         self.name = name
